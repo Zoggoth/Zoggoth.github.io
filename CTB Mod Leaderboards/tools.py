bd9e52f2bed8a53de73790b4e64cba44e9318163
Zoggoth <luke.ramsden399@gmail.com>
2023-11-01 06:28:37 +0000

    Nov 1st Data (except AutoNerf)

--- CTB Mod Leaderboards/tools.py
+++ CTB Mod Leaderboards/tools.py
@@ -4,9 +4,9 @@ import datetime
 # All the stuff that needs to be updated once a month
 # Also, remember to put the "this month" data into the "last month" folder
 # It's a giant hassle if you forget to do this
-oneMonthAgo = 1693485536
+oneMonthAgo = 1696078056
 oneYearAgo = oneMonthAgo - 525600*60
-dateName = "1st October 2023"
+dateName = "1st November 2023"
 
 
 class beatmapSet:
